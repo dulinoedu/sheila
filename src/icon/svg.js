ca056ca6686e822cc2b3206546d92399d6870bb6
cleitoncarloss <cleitoncarlos.programmer@gmail.com>
2024-10-16 10:20:37 -0300

    style: ajusta espacamento

--- src/icon/svg.js
+++ src/icon/svg.js
@@ -1,6 +1,5 @@
 const svg = {
-  adduser: () =>
-    `
+  adduser: () => `
       <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M9 12C11.7614 12 14 9.76142 14 7C14 4.23858 11.7614 2 9 2C6.23858 2 4 4.23858 4 7C4 9.76142 6.23858 12 9 12Z" stroke="currentcolor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
         <path d="M16 11V13M18 15H16V17M18 15H16M20 13H18V11" stroke="currentcolor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
