e5b7fe7a04f53a5439ba15d2075710c0cd931c59
deMGoncalves <cleber.engineer@hotmail.com>
2024-10-16 08:36:58 -0300

    feat(button): variacao do secondary

--- src/button/style.js
+++ src/button/style.js
@@ -3,29 +3,53 @@ import { css } from "@bake-js/-o-id/dom";
 function style(self) {
   return css`
     :host {
-      --background-color-primary: var(--color-primary-600);
-      --background-color-secondary: var(--color-neutral-white);
-      --background-color-tertiary: transparent;
+      --background-color-primary-solid: var(--color-primary-600);
+      --background-color-primary-ghost: var(--color-neutral-white);
+      --background-color-primary-link: transparent;
 
-      --background-color-primary-hover: var(--color-primary-700);
-      --background-color-secondary-hover: var(--color-primary-50);
-      --background-color-tertiary-hover: var(--color-primary-50);
+      --background-color-secondary-solid: var(--color-secondary-600);
+      --background-color-secondary-ghost: var(--color-neutral-white);
+      --background-color-secondary-link: transparent;
 
-      --border-color-primary: var(--color-primary-500);
-      --border-color-secondary: var(--color-primary-500);
-      --border-color-tertiary: transparent;
+      --background-color-primary-solid-hover: var(--color-primary-700);
+      --background-color-primary-ghost-hover: var(--color-primary-50);
+      --background-color-primary-link-hover: var(--color-primary-50);
 
-      --box-shadow-primary: 0 12px 0 var(--color-primary-700);
-      --box-shadow-secondary: 0 12px 0 var(--color-primary-500);
-      --box-shadow-tertiary: 0 0 0 transparent;
+      --background-color-secondary-solid-hover: var(--color-secondary-700);
+      --background-color-secondary-ghost-hover: var(--color-secondary-50);
+      --background-color-secondary-link-hover: var(--color-secondary-50);
 
-      --color-primary: var(--color-neutral-white);
-      --color-secondary: var(--color-primary-500);
-      --color-tertiary: var(--color-primary-500);
+      --border-color-primary-solid: var(--color-primary-500);
+      --border-color-primary-ghost: var(--color-primary-500);
+      --border-color-primary-link: transparent;
 
-      --color-primary-active: var(--color-neutral-white);
-      --color-secondary-active: var(--color-primary-500);
-      --color-tertiary-active: var(--color-primary-700);
+      --border-color-secondary-solid: var(--color-secondary-500);
+      --border-color-secondary-ghost: var(--color-secondary-500);
+      --border-color-secondary-link: transparent;
+
+      --box-shadow-primary-solid: 0 12px 0 var(--color-primary-700);
+      --box-shadow-primary-ghost: 0 12px 0 var(--color-primary-500);
+      --box-shadow-primary-link: 0 0 0 transparent;
+
+      --box-shadow-secondary-solid: 0 12px 0 var(--color-secondary-700);
+      --box-shadow-secondary-ghost: 0 12px 0 var(--color-secondary-500);
+      --box-shadow-secondary-link: 0 0 0 transparent;
+
+      --color-primary-solid: var(--color-neutral-white);
+      --color-primary-ghost: var(--color-primary-500);
+      --color-primary-link: var(--color-primary-500);
+
+      --color-secondary-solid: var(--color-neutral-white);
+      --color-secondary-ghost: var(--color-secondary-500);
+      --color-secondary-link: var(--color-secondary-500);
+
+      --color-primary-solid-active: var(--color-neutral-white);
+      --color-primary-ghost-active: var(--color-primary-500);
+      --color-primary-link-active: var(--color-primary-700);
+
+      --color-secondary-solid-active: var(--color-neutral-white);
+      --color-secondary-ghost-active: var(--color-secondary-500);
+      --color-secondary-link-active: var(--color-secondary-700);
 
       --font-size-large: var(--font-size-display-xs);
       --font-size-medium: var(--font-size-xl);
@@ -39,9 +63,13 @@ function style(self) {
       --padding-medium: 12px 20px;
       --padding-small: 8px 18px;
 
-      --transform-primary: translateY(12px);
-      --transform-secondary: translateY(12px);
-      --transform-tertiary: none;
+      --transform-primary-solid: translateY(12px);
+      --transform-primary-ghost: translateY(12px);
+      --transform-primary-link: none;
+
+      --transform-secondary-solid: translateY(12px);
+      --transform-secondary-ghost: translateY(12px);
+      --transform-secondary-link: none;
     }
 
     button {
@@ -77,7 +105,9 @@ function style(self) {
     }
 
     :host(:state(disabled)) {
-      opacity: 0.4; 
+      button {
+        opacity: 0.4; 
+      }
     }
   `;
 }
