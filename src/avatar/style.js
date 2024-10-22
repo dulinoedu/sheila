b28374d5e6a3f4023922a6e62d516a8b95b6d793
deMGoncalves <cleber.engineer@hotmail.com>
2024-10-22 20:05:09 -0300

    feat: estilizacao do processo fallback

--- src/avatar/style.js
+++ src/avatar/style.js
@@ -7,27 +7,41 @@ function style(self) {
       --size-large: 72px;
       display: block;
       height: var(--size-${self.size}, var(--size-small));
+      position: relative;
       width: var(--size-${self.size}, var(--size-small));
     }
 
     img {
-      align-items: center;
       aspect-ratio: 1 / 1;
+      border-radius: 50%;
+      box-sizing: border-box;
+      height: var(--size-${self.size}, var(--size-small));
+      left: 0;
+      object-fit: cover;
+      object-position: top center;
+      position: absolute;
+      top: 0;
+      width: var(--size-${self.size}, var(--size-small));
+      z-index: 2;
+    }
+
+    div {
+      align-items: center;
       background: linear-gradient(to right, #00B0AD, #00E675);
       border-radius: 50%;
       border: 3px solid rgb(49, 129, 178);
       box-sizing: border-box;
       color: white;
-      cursor: pointer;
-      display: block;
       display: flex;
       font-family: 'maven pro';
       font-weight: 500;
       height: var(--size-${self.size}, var(--size-small));
       justify-content: center;
-      object-fit: cover;
-      object-position: top center;
+      left: 0;
+      position: absolute;
+      top: 0;
       width: var(--size-${self.size}, var(--size-small));
+      z-index: 1;
     }
   `;
 }
