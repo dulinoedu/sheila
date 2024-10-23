ab087b00ce8c5208412ff2265940be0d9a724949
dijalmacavalcante <dijalma0412@gmail.com>
2024-10-23 10:32:39 -0300

    feat: cria um component de loading

---
+++ src/loading/style.js
@@ -0,0 +1,25 @@
+import { css } from "@bake-js/-o-id/dom";
+
+function style() {
+  return css`
+    :host {
+      width: 40px;
+      height: 40px;
+      border: 4px solid rgba(52, 129, 178, 0.2);
+      border-top: 4px solid #3481B2;
+      border-radius: 50%;
+      animation: spin 1s linear infinite;
+    }
+
+    @keyframes spin {
+      0% {
+        transform: rotate(0deg);
+      }
+      100% {
+        transform: rotate(360deg);
+      }
+    }
+  `;
+}
+
+export default style;
