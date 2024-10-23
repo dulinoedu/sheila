b54915b6ecb6057ea76f6aec91e1c884056e57c2
cleitoncarloss <cleitoncarlos.programmer@gmail.com>
2024-10-23 18:43:09 -0300

    refactor: ajusta a ordem de exibicao dos components

--- src/header/component.js
+++ src/header/component.js
@@ -3,13 +3,13 @@ import { html } from "@bake-js/-o-id/dom";
 function component(self) {
   return html`
     <header>
-      <lxp-logo src="//donna.sistemadulino.com.br/dulino_azul.svg" alt="Dulino"></lxp-logo>
+      <slot name="logo"></slot>
       <nav>
         <a selected href=${`//harvey.sistemadulino.com.br/app/${self.user}/${self.school}/dashboard`} target="_blank">Painel Inicial</a>
         <div></div>
         <a href=${`//harvey.sistemadulino.com.br/app/${self.user}/${self.school}/performance`} target="_blank">Desempenho</a>
       </nav>
-      <lxp-avatar src="" alt="Zezim Dev"></lxp-avatar>
+      <slot name="avatar"></slot>
     </header>
   `;
 }
