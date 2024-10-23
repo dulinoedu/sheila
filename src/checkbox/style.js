e80f940565784ce3494b9e7e04a01d5e962e0e94
deMGoncalves <cleber.engineer@hotmail.com>
2024-10-23 08:57:01 -0300

    feat: implementacao do estilo da tipografia do component

--- src/checkbox/style.js
+++ src/checkbox/style.js
@@ -2,22 +2,20 @@ import { css } from "@bake-js/-o-id/dom";
 
 function style(self) {
   return css`
-    :host {
-      cursor: pointer;
-    }
-
-    .checkbox {
-      display: flex;
+    div {
       align-items: center;
+      color: ${self.selected ? "#FB601D" : "#A6A6A6"};
+      cursor: pointer;
+      display: flex;
       gap: 4px;
-    }
 
-    .uncheck {
-      color: #A6A6A6;
-    }
-
-    .check {
-      color: #FB601D;
+      slot {
+        color: var(--color-grey-600);
+        font-family: var(--font-family-base);
+        font-size: var(--font-size-md);
+        font-weight: var(--font-weight-regular);
+        line-height: var(--line-height-md);
+      }
     }
   `;
 }
