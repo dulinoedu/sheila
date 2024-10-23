e6a567582066b16e32e2f755fc573792078f51e9
cleitoncarloss <cleitoncarlos.programmer@gmail.com>
2024-10-23 18:48:07 -0300

    refactor: nao sera redirecionado para uma nova pagina

--- src/header/component.js
+++ src/header/component.js
@@ -5,9 +5,9 @@ function component(self) {
     <header>
       <slot name="logo"></slot>
       <nav>
-        <a selected href=${`//harvey.sistemadulino.com.br/app/${self.user}/${self.school}/dashboard`} target="_blank">Painel Inicial</a>
+        <a selected href=${`//harvey.sistemadulino.com.br/app/${self.user}/${self.school}/dashboard`}>Painel Inicial</a>
         <div></div>
-        <a href=${`//harvey.sistemadulino.com.br/app/${self.user}/${self.school}/performance`} target="_blank">Desempenho</a>
+        <a href=${`//harvey.sistemadulino.com.br/app/${self.user}/${self.school}/performance`}>Desempenho</a>
       </nav>
       <slot name="avatar"></slot>
     </header>
