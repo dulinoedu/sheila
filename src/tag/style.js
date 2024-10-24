9e7feb61364a674d68994079477d84582e36a55c
cleitoncarloss <cleitoncarlos.programmer@gmail.com>
2024-10-24 13:50:10 -0300

    refactor: ajusta o estilo do button

--- src/tag/style.js
+++ src/tag/style.js
@@ -1,61 +1,7 @@
 import { css } from "@bake-js/-o-id/dom";
 
-function style(self) {
+function style() {
   return css`
-    :host {
-      --background-color-primary-solid: var(--color-primary-600);
-      --background-color-primary-ghost: var(--color-neutral-white);
-      --background-color-primary-link: transparent;
-
-      --background-color-secondary-solid: var(--color-secondary-600);
-      --background-color-secondary-ghost: var(--color-neutral-white);
-      --background-color-secondary-link: transparent;
-
-      --background-color-primary-solid-hover: var(--color-primary-700);
-      --background-color-primary-ghost-hover: var(--color-primary-50);
-      --background-color-primary-link-hover: var(--color-primary-50);
-
-      --background-color-secondary-solid-hover: var(--color-secondary-700);
-      --background-color-secondary-ghost-hover: var(--color-secondary-50);
-      --background-color-secondary-link-hover: var(--color-secondary-50);
-
-      --border-color-primary-solid: var(--color-primary-500);
-      --border-color-primary-ghost: var(--color-primary-500);
-      --border-color-primary-link: transparent;
-
-      --border-color-secondary-solid: var(--color-secondary-500);
-      --border-color-secondary-ghost: var(--color-secondary-500);
-      --border-color-secondary-link: transparent;
-
-      --color-primary-solid: var(--color-neutral-white);
-      --color-primary-ghost: var(--color-primary-500);
-      --color-primary-link: var(--color-primary-500);
-
-      --color-secondary-solid: var(--color-neutral-white);
-      --color-secondary-ghost: var(--color-secondary-500);
-      --color-secondary-link: var(--color-secondary-500);
-
-      --color-primary-solid-active: var(--color-neutral-white);
-      --color-primary-ghost-active: var(--color-primary-500);
-      --color-primary-link-active: var(--color-primary-700);
-
-      --color-secondary-solid-active: var(--color-neutral-white);
-      --color-secondary-ghost-active: var(--color-secondary-500);
-      --color-secondary-link-active: var(--color-secondary-700);
-
-      --font-size-large: var(--font-size-display-xs);
-      --font-size-medium: var(--font-size-xl);
-      --font-size-small: var(--font-size-lg);
-
-      --line-height-large: var(--line-height-display-xs);
-      --line-height-medium: var(--line-height-xl);
-      --line-height-small: var(--line-height-lg);
-
-      --padding-large: 16px 22px;
-      --padding-medium: 12px 20px;
-      --padding-small: 8px 18px;
-    }
-
     button {
       align-items: center;
       background-color: transparent;
@@ -76,17 +22,27 @@ function style(self) {
     :host(:not(:state(disabled))) {
       button {
         &:hover {
-          background-color: var(--background-color-${self.variant}-hover);
+          background-color: var(--color-primary-50);
         }
+      }
+    }
 
-        &:active {
-          color: var(--color-${self.variant}-active);
+    :host([selected]) {
+      button {
+        background-color: #3481B2;
+        color: var(--color-neutral-white);
+
+        &:hover {
+          background-color: #3481B2;
+          color: var(--color-neutral-white);
         }
       }
     }
 
     :host(:state(disabled)) {
       button {
+        border-color: var(--color-grey-200);
+        color: var(--color-grey-200);
         opacity: 0.4; 
       }
     }
