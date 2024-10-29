83eb82587d327cd32c7cb767c5d5edda75744a12
dijalmacavalcante <dijalma0412@gmail.com>
2024-10-29 14:37:45 -0300

    feat: add nova funcionalidade de troca de cor

--- src/header/header.ts
+++ src/header/header.ts
@@ -1,22 +1,23 @@
-import { define } from "@bake-js/-o-id";
-import { paint } from "@bake-js/-o-id/dom";
+import { attributeChanged, define } from "@bake-js/-o-id";
+import { paint, repaint } from "@bake-js/-o-id/dom";
+import dispatchEvent from "../dispatchEvent";
 import component from "./component";
 import style from "./style";
 
 @define("lxp-header")
 @paint(component, style)
 class Header extends HTMLElement {
-  #color;
+  #variant;
 
-  get color() {
-    return (this.#color ??= "");
+  get variant() {
+    return (this.#variant ??= "transparent");
   }
 
-  @attributeChanged("color")
-  @dispatchEvent("colorChanged")
+  @attributeChanged("variant")
+  @dispatchEvent("variantChanged")
   @repaint
-  set color(value) {
-    this.#color = value;
+  set variant(value) {
+    this.#variant = value;
   }
 
   constructor() {
--- src/header/style.js
+++ src/header/style.js
@@ -3,22 +3,26 @@ import { css } from "@bake-js/-o-id/dom";
 function style(self) {
   return css`
     :host {
-      --background-color-gradient-effect: radial-gradient(circle at top, rgba(2, 111, 198, 1) -60%, rgba(1, 54, 96, 1)) 60%;
-      --background-color-transparent: transparent;
-    }
-
-    header[oncolor] {
+      --background-gradient-effect: radial-gradient(circle at top, rgba(2, 111, 198, 1) -60%, rgba(1, 54, 96, 1)) 60%;
+      --background-transparent: transparent;
     }
 
     header {
+      background: var(--background-${self.variant});
+      display: flex;
+      justify-content: center;
+      margin: 0 auto;
+      width: 100%;
+    }
+
+    container {
       align-items: center;
       display: flex;
       height: 120px;
       justify-content: space-between;
-      margin: 0 auto;
       max-width: 1440px;
       padding: var(--spacing_inset-xs);
-      background-color: var(--background-color-${self.variant});
+      width: 100%;
     }
 
     headline slot {
